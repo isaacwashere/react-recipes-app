6981340f6d12e12c8d56270dd3a2b31814830bff
Isaac Curtiss <ijcurtiss7@gmail.com>
2019-05-16 01:48:31 -0700

    minor spacing edit

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Form = props => (
   <form onSubmit={ props.getRecipe } style={{ marginBottom: "2rem" }}>
     <input className="form__input" type="text" name="recipeName"/>
-    <button className="form__button" >Search</button>
+    <button className="form__button">Search</button>
   </form>
 );
 
